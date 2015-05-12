585c8b3918583d72e8dababb53f54db5e8354c2d
German Suarez <German.Suarez@prodigious.com>
2015-05-12 15:46:38 -0500

    [ID-1211] Some changes

--- WSSCXFClient/src/main/java/com/gersua/wsscxfclient/WSSClientMain.java
+++ WSSCXFClient/src/main/java/com/gersua/wsscxfclient/WSSClientMain.java
@@ -3,6 +3,7 @@ package com.gersua.wsscxfclient;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.ws.security.handler.WSHandlerConstants;
+
 /**
  *
  * @author gersuare
@@ -16,20 +17,20 @@ public class WSSClientMain {
     final static String signParts = timestampSignPart + ";" + bodySignPart + ";" + bstSignPart;
 
     public static void main(String[] args) {
-        
+
         /* Output interceptor config Map */
         Map<String, Object> ouputWSSProperties = new HashMap<>();
-        
+
         /* Indicates the WSS Action to execute */
-        ouputWSSProperties.put(WSHandlerConstants.ACTION, WSHandlerConstants.TIMESTAMP + " "+ WSHandlerConstants.USERNAME_TOKEN + " "+ WSHandlerConstants.SIGNATURE);
-        
+        ouputWSSProperties.put(WSHandlerConstants.ACTION, WSHandlerConstants.TIMESTAMP + " " + WSHandlerConstants.USERNAME_TOKEN + " " + WSHandlerConstants.SIGNATURE);
+
         // Signature
         ouputWSSProperties.put(org.apache.wss4j.dom.handler.WSHandlerConstants.SIGNATURE_USER, "WSSClientJKSTest");
         ouputWSSProperties.put(org.apache.wss4j.dom.handler.WSHandlerConstants.SIG_PROP_FILE, "crypto.properties");
-        
+
         // Includes a IssuerSerial signature
         ouputWSSProperties.put(org.apache.wss4j.dom.handler.WSHandlerConstants.SIG_KEY_ID, "IssuerSerial");
-        
+
         // Includes a BinarySecurityToken
         ouputWSSProperties.put(org.apache.wss4j.dom.handler.WSHandlerConstants.INCLUDE_SIGNATURE_TOKEN, "true");
 
